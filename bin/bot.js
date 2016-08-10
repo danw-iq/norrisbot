632ebf8826b449edfff0705cb884454f92445a13
danw-iq <danw@iqmetrix.com>
2016-08-10 16:00:18 -0700

    swapping db call with API call

--- bin/bot.js
+++ bin/bot.js
@@ -3,6 +3,7 @@
 'use strict'
 
 var NorrisBot = require('../lib/norrisbot.js');
+//var NorrisBot = require('../lib/danbot.js');
 
 var token = process.env.BOT_API_KEY;
 var dbPath = process.env.BOT_DB_PATH;
