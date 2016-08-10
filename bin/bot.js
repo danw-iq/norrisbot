be5d4d3927af955565b8a93b6e52961779fad502
danw-iq <danw@iqmetrix.com>
2016-08-10 10:26:51 -0700

    updated norrisbot script

--- bin/bot.js
+++ bin/bot.js
@@ -1,10 +1,10 @@
-# bin/bot.js
+/*# bin/bot.js*/
 
 'use strict'
 
 var NorrisBot = require('../lib/norrisbot.js');
 
-var token = process.env.BOT_API_KEY;
+var token = /*process.env.BOT_API_KEY;*/ 'xoxb-67883908689-l3wN8lBh5DcNsjMjF17HCbwL';
 var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
@@ -14,6 +14,7 @@ var norrisbot = new NorrisBot({
 	token: token,
 	dbPath: dbPath,
 	name: name
+
 });
 
 norrisbot.run();
