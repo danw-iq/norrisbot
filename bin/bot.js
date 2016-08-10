a7ac43f1b8e846e138f4ec654ce9ffe2a4b58c92
danw-iq <danw@iqmetrix.com>
2016-08-10 08:47:22 -0700

    finalized script

---
+++ bin/bot.js
@@ -0,0 +1,19 @@
+# bin/bot.js
+
+'use strict'
+
+var NorrisBot = require('../lib/norrisbot.js');
+
+var token = process.env.BOT_API_KEY;
+var dbPath = process.env.BOT_DB_PATH;
+var name = process.env.BOT_NAME;
+
+
+//create an instance of norrisbot and run it
+var norrisbot = new NorrisBot({
+	token: token,
+	dbPath: dbPath,
+	name: name
+});
+
+norrisbot.run();
