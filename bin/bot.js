238c67eaff48ca7646379c007cfab7dd77ef4f87
danw-iq <danw@iqmetrix.com>
2016-08-11 11:42:08 -0700

    swapped reading db with API

--- bin/bot.js
+++ bin/bot.js
@@ -2,19 +2,19 @@
 
 'use strict'
 
-var NorrisBot = require('../lib/norrisbot.js');
-//var NorrisBot = require('../lib/danbot.js');
+//var NorrisBot = require('../lib/norrisbot.js');
+var NorrisBot = require('../lib/danbot.js');
 
 var token = process.env.BOT_API_KEY;
-var dbPath = process.env.BOT_DB_PATH;
+//var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 
 //create an instance of norrisbot and run it
 var norrisbot = new NorrisBot({
 	token: token,
-	dbPath: dbPath,
 	name: name
+	//dbPath: dbPath
 
 });
 
